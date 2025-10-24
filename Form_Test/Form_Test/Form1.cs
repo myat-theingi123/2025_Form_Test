94eedd1627a62777985d3ad1751a1146ca973bef
Myat Theingi <myattheinig.myat@gmail.com>
2025-10-24 15:35:47 +0900

    october24

--- Form_Test/Form_Test/Form1.cs
+++ Form_Test/Form_Test/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,41 @@ namespace Form_Test
         public Form1()
         {
             InitializeComponent();
+
+
+            for (int i = 0; i < 6; i++)
+
+
+            {
+                for (int j = 0; j < 4; j++)
+                {
+
+
+                    TestButton testButton = new TestButton();
+
+                    Controls.Add(testButton);
+
+                    testButton.Location = new Point(50 * i, j * 50);
+
+                    testButton.Size = new Size( 50 ,30 );
+
+                    testButton.Text = "AAAAA";
+
+                }
+            }
+
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            MessageBox.Show("c#の世界へようこそ!");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("C#の世界");
         }
     }
 }
